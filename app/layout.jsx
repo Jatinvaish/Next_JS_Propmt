a84ea76528c238e60ddaba1f215b78847bae98de
Jatinvaish <1195jatinvaishnav@gmail.com>
2023-06-04 15:00:54 +0530

    bugv-- attemptm11

--- app/layout.jsx
+++ app/layout.jsx
@@ -19,6 +19,7 @@ const RootLayout = ({ children }) => (
         <main className='app'>
           <Nav />
           {children}
+          <WhatsApp />
         </main>
       </Provider>
     </body>
